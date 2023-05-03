aeed97b9c599534d136833753ea596fd328377c1
squid22 <squid22@github.com>
2023-05-03 11:44:59 -0400

    Updated the polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar.log 
 polybar bar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Bars launched..."
